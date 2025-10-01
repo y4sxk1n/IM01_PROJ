dacc589e7458396057b6ece617fd643974766390
Palagi Théo <theo.palagi2004@gmail.com>
2025-10-01 18:55:26 +0200

    test.py modifié

--- test.py
+++ test.py
@@ -3,24 +3,42 @@ import numpy as np
 
 img = cv.imread("images/lena_gray.bmp", cv.IMREAD_GRAYSCALE)
 
+def copier_im(img, r):
+    return cv.copyMakeBorder(img, r, r, r, r, borderType=cv.BORDER_CONSTANT, value=0) #on crée un copie de l'image de base pour éviter les problèmes de bord
+
 # défintion d'un patch
-def patch(img, pos, off):
-    cx, cy = pos # pos c'est les coordonnées du centre du patch
-    patch = np.zeros(2*off + 1, 2*off + 1) #on initialise à une matrice de zero pour gérer les problèmes de bords
-    for i in range(2*(off+1)):
-        for j in range(2*(off+1)):
-            patch = img[cx - off:cy + off, cy - off:cy + off] # on fait juste un bloc de pixel de la taille de notre choix
+def patch(copy_img, pos, off = 4):
+    # on utilise copy image pour ne pas avoir de problème de bord
+    cx = pos[0] + off
+    cy = pos[1] + off # pos c'est les coordonnées du centre du patch
+    patch1 = copy_img[cx - off:cy + off + 1, cy - off:cy + off +1] # on fait juste un bloc de pixel de la taille de notre choix
     return patch1
 
 # initialisation 
 def init(im1):
-    n=im1.shape 
+    H,W=im1.shape 
     offset = {}
-    for i in range (n[0]):
-        for j in range(n[1]):
-            offset[(i,j)] = (np.random.randint(0, n[1]) - j, np.random.randint(0, n[0]) - i)
+    for i in range (H):
+        for j in range(W):
+            offset[(i,j)] = (np.random.randint(0, W) - j, np.random.randint(0, H) - i)
     return offset 
+
+# fonction distance
+def distance(patch1, patch2):
+    H, W = patch1.shape
+    dist = 0
+    for i in range(H):
+        for j in range(W):
+            dist += (patch1[i,j] - patch2[i,j])**2
+    return dist
 
 # propagation et recherche aléatoire locale
 def propag(im1, im2):
+    copy1 = copier_im(im1, 4)
+    copy2 = copier_im(im2, 4)
+    H,W = im1.shape
     dist = {}
+    offset = init(im2)
+    for i in range(1,W):
+        for j in range(1,H): 
+            
